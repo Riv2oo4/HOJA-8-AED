4d2bbe919734a658508dea72d83d51b73036c2dd
Riv2oo4 <riv22500@uvg.edu.gt>
2023-03-28 09:38:53 -0600

    Progreso

---
+++ Archivo.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Archivo {
+    private String nombreArchivo;
+    private ArrayList<Proceso> lista;
+
+    public static int convertirStringAInt(String numeroString) {
+        int numeroInt;
+        try {
+            numeroInt = Integer.parseInt(numeroString);
+        } catch (NumberFormatException e) {
+            numeroInt = 0;
+        }
+        return numeroInt;
+    }
+
+    public Archivo(String nombre) {
+        this.nombreArchivo = nombre;
+        this.lista = new ArrayList<Proceso>();
+    }
+
+    public ArrayList<Proceso> leer() throws IOException {
+        File archivo = new File(nombreArchivo);
+        FileReader lector = new FileReader(archivo);
+        try (BufferedReader lectorBuffer = new BufferedReader(lector)) {
+            String linea;
+            while ((linea = lectorBuffer.readLine()) != null) {
+                String[] lista = linea.split(",");
+                if (lista.length != 3) {
+                    throw new IllegalArgumentException("Esta mal escrita la linea: " + linea);
+                }
+                String nombreProceso = lista[0];
+                String nombreUsuario = lista[1];
+                int nice = convertirStringAInt(lista[2]);
+                int prioridad = 0;
+
+                Proceso proc = new Proceso(nombreProceso, nombreUsuario, nice, prioridad);
+                this.lista.add(proc);
+
+            }
+
+        }catch(Exception e){System.out.println(e);}
+        return this.lista;
+
+    }
+}
